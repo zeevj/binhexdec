c6d40a5de2fe7ca1287683082bc2de1e5d88c2ad
Zeev Jelinek <mushmu@gmail.com>
2015-03-23 21:55:28 +0200

    remove and add mic files

---
+++ gulp/watch.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+function isOnlyChange(event) {
+  return event.type === 'changed';
+}
+
+module.exports = function(options) {
+  gulp.task('watch', ['scripts:watch', 'markups', 'inject'], function () {
+
+    gulp.watch([options.src + '/*.html', 'bower.json'], ['inject']);
+
+    gulp.watch([
+      options.src + '/{app,components}/**/*.css',
+      options.src + '/{app,components}/**/*.scss'
+    ], function(event) {
+      if(isOnlyChange(event)) {
+        gulp.start('styles');
+      } else {
+        gulp.start('inject');
+      }
+    });
+
+
+    gulp.watch(options.src + '/{app,components}/**/*.jade', ['markups']);
+
+    gulp.watch(options.src + '/{app,components}/**/*.html', function(event) {
+      browserSync.reload(event.path);
+    });
+  });
+};
